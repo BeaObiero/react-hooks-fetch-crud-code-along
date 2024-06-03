5021544bf6fce8797dd7dfb1b540cf361b99b240
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2024-06-02 21:17:11 -0700

    Completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,13 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  //fetch request as component renders
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+    .then((response) => response.json())
+    .then((items) => setItems(items)); //update state with fetched items from server
+  }, []);//empty dependency array ensures that effect only runs once
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -17,16 +24,31 @@ function ShoppingList() {
     return item.category === selectedCategory;
   });
 
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  function handleUpdateItems(updatedItem){
+    const updatedItems = items.map((items)=>{
+      if (item.id === updatedItem.id){
+        return updatedItem;
+      } else{
+        return item;
+      }
+    });
+    setItems(updatedItem);
+  }
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItems}/>
         ))}
       </ul>
     </div>
